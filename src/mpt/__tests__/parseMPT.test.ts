af538dcf801ab684cddf4bf799297f870d9270eb
sm <santimir@protonmail.com>
2022-03-26 15:00:29 +0000

    test: added basic testing for mpt

--- src/mpt/__tests__/parseMPT.test.ts
+++ src/mpt/__tests__/parseMPT.test.ts
@@ -1,13 +1,33 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
-import { parseMPT } from '../parseMPT';
+import {ComplexObject} from '../../Types';
+import { parseMPT,MPTBody } from '../parseMPT';
 
 describe('parseMPT', () => {
   it('test', () => {
     const arrayBuffer = readFileSync(
       join(__dirname, '../../__tests__/data/test/test.mpt'),
     );
-    console.log(parseMPT(arrayBuffer));
+    const result = parseMPT(arrayBuffer);
+
+    const meta = result.meta as ComplexObject; 
+    expect(Object.keys(meta)).toHaveLength(48);
+
+    const vars = result.variables as MPTBody; 
+    expect(Object.keys(vars)).toHaveLength(18);
+    
+    //some props in meta
+    expect(meta["Comments"]).toBe("")
+    expect(meta["User"]).toBe("")
+    expect(meta["flags"]).toHaveLength(5);
+   
+   //some props in vars 
+   expect(vars['Efficiency/%']).toMatchObject({
+        label: 'Efficiency',
+        units: '%',
+        isDependent: true,
+   })
+
   });
 });
