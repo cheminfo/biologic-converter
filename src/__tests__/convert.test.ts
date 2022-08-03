0383d2d41bd7c1e554c23b4cebc6700d0aa06bd6
Luc Patiny <luc@patiny.com>
2022-08-03 19:56:27 +0200

    test: remove dir from snapshot

--- src/__tests__/convert.test.ts
+++ src/__tests__/convert.test.ts
@@ -5,16 +5,14 @@ import { fileListFromPath } from 'filelist-utils';
 import { convertBioLogic as convert } from '../convert';
 
 describe('convert', () => {
-  it('test', async () => {
+  it('test mps', async () => {
     const fl = fileListFromPath(join(__dirname, 'data'));
     const directories = await convert(fl);
     // test number of directories
     expect(directories).toHaveLength(3);
     //each dir is an object with 'dir', 'mpr', 'mps', 'mpt' all optional.
-    expect(Object.keys(directories[0])).toStrictEqual([
-      'dir',
-      /*'mpr',*/ 'mps',
-    ]);
+    expect(Object.keys(directories[0])).toStrictEqual(['dir', 'mps']);
+    directories.forEach((directory) => delete directory.dir);
     expect(directories).toMatchSnapshot();
   });
 });
