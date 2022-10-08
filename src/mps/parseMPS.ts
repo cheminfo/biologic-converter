911da3365d69b1ec00f117dd570fa3d2c435cd96
Santi Miranda <santimir@protonmail.com>
2022-10-08 19:39:23 +0200

    refactor: mpt and mps getting closer to mpr

--- src/mps/parseMPS.ts
+++ src/mps/parseMPS.ts
@@ -2,30 +2,27 @@ import { TextData } from 'cheminfo-types';
 import { ensureString } from 'ensure-string';
 
 import { ComplexObject } from '../Types';
-import { camalize } from './utility/camalize';
-import { normalizeKeyValue } from './utility/normalize';
-import { getParams } from './utility/getParamsFromText';
+import { addKeyValueToResult } from '../utility/addKeyValueToResult';
 
-/*
- * these will be useful parameters depending on the technique,
- */
 /**
  * Creates an mps object from an mps file
- * The output is similar, but not the same, than `MPR.settings.variables`
- * The main difference is that MPS includes one or more techniques as an
- * whereas MPT and MPR seem to have always one
+ * The output is similar, but not the same, than `MPR.settings`
+ * MPS includes one or more techniques
  * We will know what this means when using it.
  *
  * @param data - pass the file as string, Buffer or Arraybuffer.
  * @returns JSON object representing the parsed data
  */
+
 export function parseMPS(mps: TextData): ComplexObject {
+  // file converted to an array of strings, each item a newline.
   const lines = ensureString(mps, { encoding: 'windows-1252' }).split(/\r?\n/);
+
   let result: ComplexObject = { name: lines.shift(), techniques: [] };
+
   const regex = {
-    /* regex for each case */ nothing: /^\s*$/,
+    nothing: /^\s*$/,
     keyValue: / : | :$/,
-    table: /^\w.*\s{2,}-*\w+.*\s{4,}$/,
     multiline: /^[ \t]/,
   };
 
@@ -35,41 +32,8 @@ export function parseMPS(mps: TextData): ComplexObject {
     if (regex.nothing.test(currentLine)) {
       continue;
     } else if (regex.keyValue.test(currentLine)) {
-      /* Value single or regex.multiline */
-
-      const kV: string[] = currentLine.split(regex.keyValue); //if many " : " we fix below
-      let key: string = kV[0].trim();
-      let val = '';
-
-      /* length is normally 2, it may be larger */
-      val = kV.length > 2 ? kV.slice(1).join(' : ').trim() : kV[1].trim();
-
-      if (key === 'Technique') {
-        /* Special key parsing */
-        const name = lines[++i].trim();
-        const [params, lastLineRead] = getParams(lines, ++i);
-        if (params) {
-          params.technique = name;
-          result.techniques.push(params);
-        }
-        i = lastLineRead;
-        continue; //next line, next loop
-      }
-
-      /* if not special key could be regex.multiline */
-      while (regex.multiline.test(lines[i + 1])) {
-        //use original value then
-        val = val.concat('\n', lines[++i].trim());
-      }
-
-      const [newKey, newVal] = normalizeKeyValue(key, val);
-      result[newKey] = newVal;
-    } else if (regex.table.test(currentLine)) {
-      /* for not k : v */
-      //regex.table
-      const kV: string[] = currentLine.split(/\s{2,}/);
-      const [key, val] = [kV[0].trim(), kV.slice(1).join('  ').trim()];
-      result[key] = val;
+      //function updates the index bc some values are multiline
+      [result, i] = addKeyValueToResult(result, lines, currentLine, regex, i, "MPS");
     } else if (Array.isArray(result.flags)) {
       result.flags.push(currentLine);
     } else {
